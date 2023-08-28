3e134a2759365669d7a3322608cd0dd63aae6528
ParkMini <parkteammini@gmail.com>
2023-08-28 15:13:24 +0900

    Initial Commit

---
+++ src/main/java/kr/pah/comwiki/ComwikiApplication.java
@@ -0,0 +1,13 @@
+package kr.pah.comwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComwikiApplication.class, args);
+	}
+
+}
