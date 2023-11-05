173f92debccc05ef190f15afdb185822cb7635e7
ParkMini <parkteammini@gmail.com>
2023-11-05 20:58:59 +0900

    로그인 및 회원가입 DTO

---
+++ src/main/java/kr/pah/comwiki/dto/user/LoginDto.java
@@ -0,0 +1,13 @@
+package kr.pah.comwiki.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String username;
+    private String password;
+}
